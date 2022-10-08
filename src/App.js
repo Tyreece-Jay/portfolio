33306f59720ba3648d2aaeec0007e7d1b5861a09
Tyreece-Jay <60540726+Tyreece-Jay@users.noreply.github.com>
2022-10-08 22:57:06 +0100

    Started changes for new design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Downarrow from './icons8-expand-arrow-96.png';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
 import './App.css';
 
 function App() {
@@ -19,17 +17,12 @@ function App() {
         <img src={Downarrow} alt="down arrow" />
       </header>
       <body>
-        <div className="About-text">
-          <p aria-label='about text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div>
+          <p className="About-text" aria-label='about text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div>
           <h4 className="Skills-header">PROFESSIONAL SKILLS</h4>
           <div className="Skills">
-            <CircularProgressbar value={66} text={`React`} />
-            <CircularProgressbar value={66} text={`TypeScript`} />
-            <CircularProgressbar value={66} text={`HTML & CSS`} />
-            <CircularProgressbar value={66} text={`C#`} />
-            <CircularProgressbar value={66} text={`SQL`} />
           </div>
         </div>
       </body>
--- src/App.test.js
+++ src/App.test.js
@@ -1,49 +1,42 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders name', () => {
-  render(<App />);
-  const name = screen.getByText("TYREECE SIMPSON");
-  expect(name).toBeInTheDocument();
-});
-
-test('renders headline', () => {
-  render(<App />);
-  const partOne = screen.getByText("Full-Stack Software Engineer");
-  const partTwo = screen.getByText("Front-End Specialist");
-  expect(partOne).toBeInTheDocument();
-  expect(partTwo).toBeInTheDocument();
-});
-
-test('renders down arrow', () => {
-  render(<App />);
-  const downArrow = screen.getByAltText("down arrow");
-  expect(downArrow).toBeInTheDocument();
-});
-
-test('renders about text', () => {
-  render(<App />);
-  const aboutText = screen.getByLabelText("about text");
-  expect(aboutText).toBeInTheDocument();
-});
-
-test('renders skills title', () => {
-  render(<App />);
-  const skillsTitle = screen.getByText("PROFESSIONAL SKILLS");
-  expect(skillsTitle).toBeInTheDocument();
-});
-
-test('renders skills', () => {
-  render(<App />);
-  expect(screen.getByText("React")).toBeInTheDocument();
-  expect(screen.getByText("TypeScript")).toBeInTheDocument();
-  expect(screen.getByText("HTML & CSS")).toBeInTheDocument();
-  expect(screen.getByText("C#")).toBeInTheDocument();
-  expect(screen.getByText("SQL")).toBeInTheDocument();
-});
-
-test('renders footer text', () => {
-  render(<App />);
-  const footerText = screen.getByText("Copyright © 2022 Tyreece Simpson - All Rights Reserved.");
-  expect(footerText).toBeInTheDocument();
+describe('renders', () => {
+  test('name', () => {
+    render(<App />);
+    const name = screen.getByText("TYREECE SIMPSON");
+    expect(name).toBeInTheDocument();
+  });
+  
+  test('headline', () => {
+    render(<App />);
+    const partOne = screen.getByText("Full-Stack Software Engineer");
+    const partTwo = screen.getByText("Front-End Specialist");
+    expect(partOne).toBeInTheDocument();
+    expect(partTwo).toBeInTheDocument();
+  });
+  
+  test('down arrow', () => {
+    render(<App />);
+    const downArrow = screen.getByAltText("down arrow");
+    expect(downArrow).toBeInTheDocument();
+  });
+  
+  test('about text', () => {
+    render(<App />);
+    const aboutText = screen.getByLabelText("about text");
+    expect(aboutText).toBeInTheDocument();
+  });
+  
+  test('skills title', () => {
+    render(<App />);
+    const skillsTitle = screen.getByText("PROFESSIONAL SKILLS");
+    expect(skillsTitle).toBeInTheDocument();
+  });
+  
+  test('footer text', () => {
+    render(<App />);
+    const footerText = screen.getByText("Copyright © 2022 Tyreece Simpson - All Rights Reserved.");
+    expect(footerText).toBeInTheDocument();
+  });
 });
