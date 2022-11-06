5d7094844f224965371b7f999f94394432ac5c02
Tyreece-Jay <60540726+Tyreece-Jay@users.noreply.github.com>
2022-11-06 21:56:54 +0000

    refactored progress bar

--- src/sections/components/progressbar.tsx
+++ src/sections/components/progressbar.tsx
@@ -3,22 +3,14 @@ import React from "react";
 export const ProgressBar = (props: { progress: any; }) => {
   const { progress } = props;
 
-  const containerStyles = {
-    width: '100%',
-    backgroundColor: "#3A3E41",
-    height: 20
-  };
-
-  const fillerStyles = {
+  const dimensions = {
     height: 20,
     width: `${progress}%`,
-    backgroundColor: "#F6F7EB",
-    transition: 'width 1s ease-in-out'
   };
 
   return (
-    <div style={containerStyles}>
-      <div style={fillerStyles}>
+    <div className="progress-container">
+      <div className="progress-fill" style={dimensions}>
       </div>
     </div>
   );
