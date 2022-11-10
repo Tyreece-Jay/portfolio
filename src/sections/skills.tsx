358ddebe4b9c3e4c1628469fa45b537d0cde551d
Tyreece-Jay <tyreece.jay@gmail.com>
2022-11-10 11:21:51 +0000

    #8

--- src/sections/skills.tsx
+++ src/sections/skills.tsx
@@ -6,10 +6,12 @@ export default function Skills() {
   return <div className="skills">
     <h4 className="skills-headline">Constantly Curious</h4>
     <h3 className="skills-title">PROFESSIONAL SKILLS</h3>
-    <div className='skill-bars'>
+    <div className='frontend-skills'>
       {SkillBar("javascript & typescript", SkillPercentage.JavascriptAndTypescript)}
       {SkillBar("react", SkillPercentage.React)}
       {SkillBar("html & css", SkillPercentage.HtmlAndCss)}
+    </div>
+    <div className='backend-skills'>
       {SkillBar("c#", SkillPercentage.CSharp)}
       {SkillBar("sql", SkillPercentage.Sql)}
       {SkillBar(".net", SkillPercentage.DotNet)}
