27adc15e0cc9f6e99b821881ac91c12b5cab3298
Tyreece-Jay <60540726+Tyreece-Jay@users.noreply.github.com>
2023-01-20 22:15:46 +0000

    .

--- src/sections/components/typewriter.tsx
+++ src/sections/components/typewriter.tsx
@@ -2,27 +2,27 @@ function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+export const write = async () => {
 const phrases = ["Full-Stack Software Engineer", "Front-End Specialist"];
-const el = document.getElementById("typewriter");
+const element = document.getElementById("typewriter");
 
 let sleepTime = 100;
 let curPhraseIndex = 0;
 
-export const write = async () => {
-  while (true) {
+while (true) {
     let curWord = phrases[curPhraseIndex];
 
     for (let i = 0; i < curWord.length; i++) {
-      if (el != null)
-        el.innerText = curWord.substring(0, i + 1);
+      if (element != null)
+        element.innerText = curWord.substring(0, i + 1);
       await sleep(sleepTime);
     }
 
     await sleep(sleepTime * 10);
 
     for (let i = curWord.length; i > 0; i--) {
-      if (el != null)
-        el.innerText = curWord.substring(0, i - 1);
+      if (element != null)
+        element.innerText = curWord.substring(0, i - 1);
       await sleep(sleepTime);
     }
 
--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
     } else {
       gradient.pause();
     }
-    write();
+    write()
   }, []);
 
   return <div className='header'>
