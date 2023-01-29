34a43637497af4d72498832d075512ddec0204e2
Tyreece-Jay <60540726+Tyreece-Jay@users.noreply.github.com>
2023-01-29 18:23:00 +0000

    #17

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -13,10 +13,15 @@ export default function Skills() {
         <div className={styles.ts}><h4>TypeScript/JavaScript</h4></div>
         <div className={styles.ui}><h4>UI/UX</h4></div>
       </div>
-      <div>
-        <div><h4>C#</h4></div>
-        <div><h4>SQL</h4></div>
-        <div><h4>.NET</h4></div>
+      <div className={styles.backend}>
+        <div className={styles.cSharp}><h4>C#</h4></div>
+        <div className={styles.dotNet}><h4>.NET</h4></div>
+        <div className={styles.sql}><h4>SQL</h4></div>
+      </div>
+      <div className={styles.backend}>
+        <div className={styles.cSharp}><h4>TDD</h4></div>
+        <div className={styles.dotNet}><h4>DDD</h4></div>
+        <div className={styles.sql}><h4>BDD</h4></div>
       </div>
     </div>
   </div>;
