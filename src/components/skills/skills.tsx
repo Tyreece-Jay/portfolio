18148ffb76f6f3d3a7207826c30943503960ab3f
Tyreece-Jay <60540726+Tyreece-Jay@users.noreply.github.com>
2023-01-30 23:38:25 +0000

    #17

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -10,28 +10,28 @@ export default function Skills() {
     <h4>Constantly Curious</h4>
     <h3 className={styles.title}>PROFESSIONAL SKILLS</h3>
     <div className={styles.content}>
-        <h4 className={styles.subHeading}><img src={frontend} alt="Front-End" />Front-End</h4>
-        <div className={styles.frontend}>
-          <div className={styles.react}><h4>React</h4></div>
-          <div className={styles.html}><h4>HTML</h4></div>
-          <div className={styles.css}><h4>CSS</h4></div>
-          <div className={styles.ts}><h4>TypeScript/JavaScript</h4></div>
-          <div className={styles.ui}><h4>UI/UX</h4></div>
-        </div>
-        <h4 className={styles.subHeading}><img src={backend} alt="Back-End" />Back-End</h4>
-        <div className={styles.backend}>
-          <div className={styles.cSharp}><h4>C#</h4></div>
-          <div className={styles.dotNet}><h4>.NET</h4></div>
-          <div className={styles.sql}><h4>SQL</h4></div>
-        </div>
-          <h4 className={styles.subHeading}><img src={practices} alt="Practices" />Practices</h4>
-          <div className={styles.practices}>
-            <div className={styles.tdd}><h4>TDD</h4></div>
-            <div className={styles.ddd}><h4>DDD</h4></div>
-            <div className={styles.bdd}><h4>BDD</h4></div>
-            <div className={styles.programming}><h4>Pair Programming</h4></div>
-            <div className={styles.devops}><h4>DevOps</h4></div>
-          </div>
+      <h4 className={styles.subHeading}><img src={frontend} alt="Front-End" />Front-End</h4>
+      <div className={styles.frontend}>
+        <h4>React</h4>
+        <h4>HTML</h4>
+        <h4>CSS</h4>
+        <h4>TypeScript/JavaScript</h4>
+        <h4>UI/UX</h4>
+      </div>
+      <h4 className={styles.subHeading}><img src={backend} alt="Back-End" />Back-End</h4>
+      <div className={styles.backend}>
+        <h4>C#</h4>
+        <h4>.NET</h4>
+        <h4>SQL</h4>
+      </div>
+      <h4 className={styles.subHeading}><img src={practices} alt="Practices" />Practices</h4>
+      <div className={styles.practices}>
+        <h4>TDD</h4>
+        <h4>DDD</h4>
+        <h4>BDD</h4>
+        <h4>Pair Programming</h4>
+        <h4>DevOps</h4>
+      </div>
     </div>
   </div>;
 }
